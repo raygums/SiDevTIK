951d5d257763057e3d3eaeb9749171970f33c04a
firmanfarelrichardo <farelpasaribu04@gmail.com>
2026-01-09 11:03:40 +0700

    clear comment after test

--- domaintik/vite.config.js
+++ domaintik/vite.config.js
@@ -18,25 +18,3 @@ export default defineConfig({
         },
     },
 });
-
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-
-// export default defineConfig({
-//     server: {
-//         host: '0.0.0.0', // Izinkan akses dari luar container (laptop Anda)
-//         port: 5173,      // Port standar Vite
-//         hmr: {
-//             host: 'localhost', // Browser di laptop akan connect ke sini
-//         },
-//         watch: {
-//             usePolling: true, // Wajib untuk Docker di Windows/WSL agar perubahan file terdeteksi
-//         },
-//     },
-//     plugins: [
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         }),
-//     ],
-// });
