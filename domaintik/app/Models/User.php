fd77531375e9cbb328d61ea900af15dcb8448c57
firmanfarelrichardo <farelpasaribu04@gmail.com>
2026-01-12 14:50:04 +0700

    fix: update migration schema creation logic

--- domaintik/app/Models/User.php
+++ domaintik/app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App/Enums/UserRole;
+use App\Enums\UserRole;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
