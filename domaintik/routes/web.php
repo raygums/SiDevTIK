ff04bfee195ebd8216583e3e00f6efcb30237f5b
Aidil1705 <aidil1705fikri@gmail.com>
2026-01-12 16:22:20 +0700

    dashboard verifikator

--- domaintik/routes/web.php
+++ domaintik/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/verifikator', function () {
+    return view('verifikator.dashboard');
+});
